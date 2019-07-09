e14ff8bd854f15a605bb863c00dc8914d39a7c2b
Bartel Verkruijssen <bartel@verkruijssen.com>
2019-07-09 13:18:39 +0200

    before removing blog

--- doczrc.js
+++ doczrc.js
@@ -4,7 +4,7 @@ export default {
   title: 'Sevi Library',
   base: '/docs',
   ignore: ['**/blog/**', 'readme.md'],
-  menu: ['Getting Started', 'For anyone', 'For group admin'],
+  menu: ['Getting Started', 'For anyone', 'For groups', 'For admins'],
   themeConfig: {
     colors: {
       primary: colors.primary,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,68 +4,13 @@ import React from 'react'
 import { graphql } from 'gatsby'
 // import styled from 'styled-components'
 import { navigate } from 'gatsby'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+// import Layout from '../components/layout'
+// import SEO from '../components/seo'
 // import { rhythm } from '../utils/typography'
 
-class BlogIndex extends React.Component {
+export default class BlogIndex extends React.Component {
   render() {
-    navigate('/docs')
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        {/* {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <PostItem key={node.fields.slug}>
-              <PostTitle fontSize={5}>
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </PostTitle>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </PostItem>
-          )
-        })} */}
-      </Layout>
-    )
+    navigate('/docs/')
+    return null
   }
 }
-
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
-    }
-  }
-`
