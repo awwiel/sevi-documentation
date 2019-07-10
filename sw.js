e48d8741b29e086e79fa06fd8b30177e7d85aa0d
Bartel Verkruijssen <bartel@verkruijssen.com>
2019-07-10 09:53:08 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-180f8f1be3ac17c5765e.js"
+    "url": "webpack-runtime-08f5c91d3b5ab42a0044.js"
   },
   {
     "url": "styles.e0a640a6ca692e618d67.css"
@@ -35,14 +35,14 @@ self.__precacheManifest = [
     "url": "styles-fc13ea2fdec5491330da.js"
   },
   {
-    "url": "app-32cefd3b28aaf7f08a6e.js"
+    "url": "app-3fd47246b052b02da428.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-82d8997f6e3730d6c1b5.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "99401fab5969e99f726af7a2a54075fc"
+    "revision": "02aa31d54d783d7814b11e6453805dcd"
   },
   {
     "url": "0-a158e74afe949fdad1da.js"
